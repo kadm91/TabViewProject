7a7e2fbf0170992dca0fb3af2ca570d75d809c81
Kevin Martinez <82669898+kadm91@users.noreply.github.com>
2024-01-12 20:57:26 -0700

    Initial Commit

---
+++ TabViewProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TabViewProject
+//
+//  Created by Kevin Martinez on 1/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TabViewProject/TabViewProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabViewProjectApp.swift
+//  TabViewProject
+//
+//  Created by Kevin Martinez on 1/12/24.
+//
+
+import SwiftUI
+
+@main
+struct TabViewProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
