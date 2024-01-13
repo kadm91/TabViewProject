47e052a650d3cb4171a2b46c8a3ebbe2e764a002
Kevin Martinez <82669898+kadm91@users.noreply.github.com>
2024-01-12 21:08:51 -0700

    Review of TabView

--- TabViewProject/ContentView.swift
+++ TabViewProject/ContentView.swift
@@ -9,13 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView {
+            Text("Hello World")
+                .tabItem {
+                    Label("Home", systemImage: "house")
+                }
+            
+            Text("Profile View")
+                .tabItem {
+                    Label("Profile", systemImage: "person.circle.fill")
+                }
         }
-        .padding()
+        .tabViewStyle(.page)
     }
 }
 
